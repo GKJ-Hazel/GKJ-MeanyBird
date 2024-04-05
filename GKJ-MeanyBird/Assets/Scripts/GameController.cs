75d6c52570025fd4af849f6612ce93cb2477f683
willbooker350 <codeninjascna4@gmail.com>
2024-04-05 17:36:05 -0400

    Start

---
+++ GKJ-MeanyBird/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+//Game manager object
+[Header("Game Controller Object for controlling the game")]
+public GameController gameController;
+[Header("Default Velocity")]
+public float velocity = 5
